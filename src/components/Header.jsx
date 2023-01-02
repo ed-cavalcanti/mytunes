08d7154b3fa5e53bf728c79bcb8477da1ba6ee59
ed-cavalcanti <edney2809@gmail.com>
2023-01-02 17:56:24 -0300

    [style] header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
+import './Header.css';
 
 export default class Header extends Component {
   state = {
@@ -24,14 +25,16 @@ export default class Header extends Component {
           loading
             ? <Loading />
             : (
-              <div>
-                <span>Olá, </span>
-                <span data-testid="header-user-name">{name.name}</span>
-                <ul>
+              <div className="header-content">
+                <div>
+                  <span className="hi">Olá, </span>
+                  <span className="hi" data-testid="header-user-name">{name.name}</span>
+                </div>
+                <div className="navbar">
                   <Link to="/search" data-testid="link-to-search">Pesquisar</Link>
                   <Link to="/favorites" data-testid="link-to-favorites">Favoritos</Link>
                   <Link to="/profile" data-testid="link-to-profile">Perfil</Link>
-                </ul>
+                </div>
               </div>
             )
         }
